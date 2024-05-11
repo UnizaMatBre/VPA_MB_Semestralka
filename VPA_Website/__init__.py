0540068fe29268241ea19b23cb4490f55c054504
UnizaMatBre <breznican1@stud.uniza.sk>
2024-05-11 19:53:08 +0200

    Added and initialized SQLAlchemy instance

--- VPA_Website/__init__.py
+++ VPA_Website/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 
 class Localization:
@@ -7,16 +8,16 @@ class Localization:
 
 def create_app(app_config):
     app = Flask(__name__)
-
+    db = SQLAlchemy()
 
     app.config.from_object(app_config)
+    db.init_app(app)
 
+    from models import create_models
+    models = create_models(db)
 
     # load localization (implement translations
     localization = Localization()
-
-    from models import create_models
-    models = create_models(None)
 
     @app.route("/")
     @app.route("/index")
