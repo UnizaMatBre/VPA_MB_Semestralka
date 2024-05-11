fba9f773da0c08024a89c6db5bacc50acfce0a22
UnizaMatBre <breznican1@stud.uniza.sk>
2024-05-11 21:47:08 +0200

    Created User model

--- VPA_Website/models.py
+++ VPA_Website/models.py
@@ -3,6 +3,10 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 def create_models(db):
     class _Namespace:
-        pass
+        class User(db.Model):
+            id: Mapped[int] = mapped_column(primary_key=True)
+            username: Mapped[str] = mapped_column(nullable=False, unique=True)
+            passhash: Mapped[str] = mapped_column(nullable=False)
+
 
     return _Namespace
