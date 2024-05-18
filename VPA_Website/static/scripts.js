14af507f9814b059e0ddb8662b4d68a7b8ba0c44
UnizaMatBre <breznican1@stud.uniza.sk>
2024-05-18 17:55:18 +0200

    Added fetching utility function

--- VPA_Website/static/scripts.js
+++ VPA_Website/static/scripts.js
@@ -1,3 +1,36 @@
+/**
+* Function that simplifies handling of fetch promises
+*
+* \fetchPromise : promise returned by fetch call
+* \correctHandler    : handler for response without errors. Accepts body of response in json
+* \apiErrorHandler   : handler for response with error from api. Accepts response itself
+* \fetchErrorHandler : handler for errors from fetch (like network error). Accepts response itself
+*/
+handleFetchPromise = function(fetchPromise, correctHandler, apiErrorHandler, fetchErrorHandler) {
+    fetchPromise
+        .then((response) => {
+            // handle error response
+	        if(!response.ok) {
+	            return Promise.reject(response);
+	        }
+
+            // turn ok response into json
+	        return response.json();
+
+        })
+        .then(correctHandler)
+        .catch((error) => {
+            if(typeof error.json === "function") {
+                apiErrorHandler(error);
+            }
+            else {
+                fetchErrorHandler(error);
+            }
+        });
+}
+
+
+
 showErrorMessage = function(messageText) {
     const messageSection = document.getElementById("message-section");
 
