71415727e7bdd7d594edb746fb8d9c86dfe14636
UnizaMatBre <breznican1@stud.uniza.sk>
2024-05-22 21:05:35 +0200

    Refactored user views into their own blueprint

--- VPA_Website/user_views.py
+++ VPA_Website/user_views.py
@@ -10,4 +10,64 @@ def create_user_blueprint(db, models, localization):
         template_folder="templates"
     )
 
+    @user_blueprint.route("/user/<int:user_id>", methods=["GET"])
+    @jwt_required(optional=True)
+    def user_by_id_get(user_id):
+        """Returns profile of user that has passed id"""
+
+        # find user with this id
+        result: models.User = db.session.execute(
+            db.select(models.User).filter_by(id=user_id)
+        ).scalar_one_or_none()
+
+        return render_template("user_profile.html", auth_user=get_current_user(), localization=localization,
+                               user_obj=result)
+
+    @user_blueprint.route("/user", methods=["POST"])
+    def user_post():
+        """Creates new user using data from request"""
+
+        # extract inputs
+        input_username = request.json.get("username", None)
+        input_password = request.json.get("password", None)
+
+        # check if fields are actually present
+        if input_username is None:
+            return jsonify({"msg": "Missing username"}), 422
+
+        # check if fields are actually present 2
+        if input_password is None:
+            return jsonify({"msg": "Missing password"}), 422
+
+        # find user with this exact same username
+        result: models.User = db.session.execute(
+            db.select(models.User).filter_by(username=input_username)
+        ).scalar_one_or_none()
+
+        # user already exists, return error
+        if result is not None:
+            return jsonify({"msg": "User already exists"}), 409
+
+        # generate password hash
+        input_passhash = bcrypt.hashpw(
+            password=bytes(input_password, "utf-8"),
+            salt=bcrypt.gensalt()
+        )
+
+        # create user
+        new_user = models.User(
+            username=input_username,
+            passhash=input_passhash
+        )
+
+        db.session.add(new_user)
+
+        # create response
+        response = make_response(jsonify("{}"), 201)
+        response.headers["Location"] = "/user/" + str(new_user.id)
+
+        db.session.commit()
+
+        return response
+
     return user_blueprint
